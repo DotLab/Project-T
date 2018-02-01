af021bbaf561977bcaae00de57b914c69b6b0a13
AK2806 <18268291381@163.com>
2018-02-01 16:34:18 +0800

    Add scene classes  & JSON.net
    Add classes SceneManger, SceneListFile, Scene, SceneFile & library
    JSON.net

---
+++ Assets/AK2806/LogicLibrary/GameLogic/GameLogic/Scene/Scene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameLogic.Scene
+{
+    public struct SceneFile { // persistent
+        public struct Action {
+            public enum ActionType
+            {
+                Tween, Cutscene, SetBGImg, SetBGAnimation, AddImg, RmImg,
+                AddAnimation, RmAnimation, SetEffect, AddChara, CharaBehave,
+                PlayBGM, StopBGM, PlaySE, ShowTip, ShowMessage, Battle
+            }
+
+            public struct Next {
+                public string condition;
+                public int index;
+                public string tip;
+            }
+
+            public ActionType actionType;
+            public string param;
+            public bool wait;
+            public List<Next> nexts;
+        }
+
+        public string fileType;
+        public List<Action> actions;
+    }
+
+    public class Scene
+    {
+        private SceneFile mScene;
+
+        public Scene()
+        {
+        }
+    }
+}
---
+++ Assets/AK2806/LogicLibrary/GameLogic/GameLogic/SceneManger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GameLogic
+{
+    public struct SceneListFile { // persistent
+        public struct Router {
+            public struct From {
+                public string scene;
+                public string outcome; // reference to the outcome’s name in scene file
+            }
+
+            public From from;
+            public string to; // reference to the name of scene
+        }
+
+        public string fileType;
+        public List<Scene.SceneFile> scenes;
+        public string startup; // reference to the name of scene
+        public List<Router> routers;
+    }
+
+    public class SceneManger
+    {
+        private SceneListFile mSceneList;
+
+        public SceneManger()
+        {
+        }
+
+        public void Load(string json) {
+            this.mSceneList = JsonConvert.DeserializeObject<SceneListFile>(json);
+        }
+    }
+}
