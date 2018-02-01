90d64b2e5ce3cb6ae32e70420b1936eebaa78120
AK2806 <18268291381@163.com>
2018-02-01 14:38:20 +0800

    Create logic solution
    New empty console solution

---
+++ Assets/AK2806/LogicLibrary/GameLogic/GameLogic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameLogic
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
