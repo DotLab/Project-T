7b81d1a0be89ed6a81c7177f050b898b4fbf21db
AK2806 <18268291381@163.com>
2018-02-07 15:28:46 +0800

    Replace ClearScript.V8 with Jint
    Replace ClearScript.V8 with Jint

--- Assets/AK2806/LogicLibrary/GameLogic/GameLogic/Program.cs
+++ Assets/AK2806/LogicLibrary/GameLogic/GameLogic/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-
+using Newtonsoft.Json;
+using Jint;
 namespace GameLogic
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string json = @"
+                {
+                    'name':'John',
+                    'age':32,
+                    'phoneNum':[
+                        1234567,
+                        9876543
+                    ]
+                }
+            ";
+            object test = JsonConvert.DeserializeObject(json);
+            Console.WriteLine(test.GetHashCode());
+
+            var engine = new Engine()
+        .SetValue("log", new Action<object>(Console.WriteLine))
+        ;
+
+            engine.Execute(@"
+      (function(){ 
+        log('Hello World');
+      })();
+      
+    ");
         }
     }
 }
