b56b1591e7c94eaa193fa2cab6af6c5b5881baf4
Chengyu Qian <18268291381@163.com>
2018-03-12 19:18:51 +0800

    Create Class Dice
    Create Class Dice

--- Assets/AK2806/LogicLibrary/GameLogic/GameLogic/Program.cs
+++ Assets/AK2806/LogicLibrary/GameLogic/GameLogic/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using Newtonsoft.Json;
 using Jint;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using GameLogic.Utilities;
+
 namespace GameLogic
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
+            /*
             string json = @"
                 {
                     'name':'John',
@@ -30,6 +36,49 @@ namespace GameLogic
               })();
               
             ");
+
+
+            IFormatter formatter = new BinaryFormatter();
+
+            Class3 testSerialize = new Class3();
+            testSerialize.interface1s.Add(new Class1());
+            testSerialize.interface1s.Add(new Class2());
+            Stream stream = new FileStream(@"D:\MyFile.bin", FileMode.Create,
+            FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, testSerialize);
+            stream.Close();
+            
+            Stream stream2 = new FileStream(@"D:\MyFile.bin", FileMode.Open,
+            FileAccess.Read, FileShare.Read);
+            Class3 obj = (Class3)formatter.Deserialize(stream2);
+            stream2.Close();
+
+            foreach (Interface1 i in obj.interface1s)
+            {
+                System.Console.WriteLine(i.Desc());
+            }
+            */
+            /*
+            DicePoint[] dicePoints = new DicePoint[2];
+            dicePoints[0]= new DicePoint(0, 0.8);
+            dicePoints[1]= new DicePoint(1, 0.8);
+            Dice dice = Dice.Create(dicePoints);
+            */
+            /*
+            int[] points = { 1, 2, 3, 6, 7, 8 };
+            Dice dice = Dice.Create(DiceType.Create(points));
+            StreamWriter stream2 = new StreamWriter(@"D:\test.txt");
+            int i = 0;
+            while (i++ < 1000)
+            {
+                stream2.WriteLine(dice.Roll(1));
+            }
+            stream2.Close();
+            */
+            System.Console.Write("请按任意键继续...");
+            System.Console.ReadKey();
+
+            
         }
     }
 }
---
+++ Assets/AK2806/LogicLibrary/GameLogic/GameLogic/Utilities/Dice.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace GameLogic.Utilities
+{
+    struct DicePoint
+    {
+        public int value;
+        public double weight;
+        
+        public DicePoint(int v, double w)
+        {
+            this.value = v;
+            this.weight = w;
+        }
+    }
+
+    struct DiceType
+    {
+        public DicePoint[] range;
+
+        public static DiceType Create(int min, int max)
+        {
+            DiceType ret;
+            int count = max - min + 1;
+            double weight = 1.0 / count;
+            ret.range = new DicePoint[count];
+            for (int i = 0; i < count; ++i)
+            {
+                ret.range[i] = new DicePoint(min + i, weight);
+            }
+            return ret;
+        }
+
+        public static DiceType Create(int[] points)
+        {
+            DiceType ret;
+            double weight = 1.0 / points.Length;
+            ret.range = new DicePoint[points.Length];
+            for (int i = 0; i < points.Length; ++i)
+            {
+                ret.range[i] = new DicePoint(points[i], weight);
+            }
+            return ret;
+        }
+    }
+
+    class Dice
+    {
+        public static Dice Create(DicePoint[] points)
+        {
+            Dice ret = new Dice();
+            ret.CreateDiceType(points);
+            return ret;
+        }
+
+        public static Dice Create(DiceType diceType)
+        {
+            Dice ret = new Dice();
+            ret.diceType = diceType;
+            return ret;
+        }
+
+        private Random generator;
+        private DiceType diceType;
+
+        public DiceType DiceType
+        {
+            get
+            {
+                return this.diceType;
+            }
+            set
+            {
+                this.diceType = value;
+            }
+        }
+
+        public Dice()
+        {
+            this.generator = new Random();
+        }
+
+        public int Roll(int number)
+        {
+            int ret = 0;
+            for (int c = 1; c <= number; ++c)
+            {
+                double rand = this.generator.NextDouble();
+                double step = 0.0;
+                int value = this.diceType.range[this.diceType.range.Length - 1].value;
+                for (int i = 0; i < this.diceType.range.Length; ++i)
+                {
+                    double weight = this.diceType.range[i].weight;
+                    if (rand >= step && rand < weight + step)
+                    {
+                        value = this.diceType.range[i].value;
+                        break;
+                    }
+                    step += weight;
+                }
+                ret += value;
+            }
+            return ret;
+        }
+
+        public void CreateDiceType(DicePoint[] points)
+        {
+            this.diceType.range = points;
+        }
+
+    }
+}
